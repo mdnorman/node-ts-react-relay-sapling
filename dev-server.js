519a8d760400a11230e924e9886b1e7dbd8d2fb5
Michael D. Norman <michael.norman@denormans.com>
2018-09-26 22:00:39 -0500

    fix graphql url

--- dev-server.js
+++ dev-server.js
@@ -4,7 +4,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const WEBPACK_SERVER_PORT = process.env.WEBPACK_SERVER_PORT || 5000;
 
 const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 3000;
-const GRAPHQL_URL = process.env.GRAPHQL_URL || `http://localhost:${GRAPHQL_PORT}/graphql`;
+const GRAPHQL_URL = process.env.GRAPHQL_URL || `http://localhost:${GRAPHQL_PORT}`;
 
 const config = require('./webpack.config')({}, {});
 const options = {
